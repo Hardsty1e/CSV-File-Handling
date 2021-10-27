4fa964597f5cbd69fb319f74b4f6b075a32f5bb0
Hardsty1e <59625228+Hardsty1e@users.noreply.github.com>
2021-10-27 20:44:39 +0530

    Count the number of rows(entries) in a csv file

---
+++ csv_file_length.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 30 16:07:53 2021
+
+@author: sanchit.thakur.in
+"""
+
+import csv
+
+file = open("FC_Score_variable_Aug20.csv000")
+reader = csv.reader(file)
+lines= len(list(reader))
+print(lines)
