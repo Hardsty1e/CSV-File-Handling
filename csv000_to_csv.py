601f590cfe1236cf00dc500fcd94ff413e281363
Hardsty1e <59625228+Hardsty1e@users.noreply.github.com>
2021-10-28 17:08:32 +0530

    Update csv000_to_csv.py

--- csv000_to_csv.py
+++ csv000_to_csv.py
@@ -7,6 +7,6 @@ Created on Thu Sep 30 17:25:41 2021
 
 import pandas as pd
 
-dataframe1 = pd.read_csv("pe_ruts_20_oct.csv000")
+dataframe1 = pd.read_csv("File_name.csv000")
 
-dataframe1.to_csv("pe_ruts_20_oct.csv", index = False)     # index = false, otherwise new empty row will be added on top
+dataframe1.to_csv("File_name.csv", index = False)     # index = false, otherwise new empty row will be added on top
