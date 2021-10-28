1ed8d65377d32ae27470cba0ab7664c5ef4fa288
Hardsty1e <59625228+Hardsty1e@users.noreply.github.com>
2021-10-28 17:07:49 +0530

    Update File_to_chunks.py

--- File_to_chunks.py
+++ File_to_chunks.py
@@ -10,6 +10,6 @@ import pandas as pd
 chunk_size = 1000000    # Chunk Size
 batch_no = 1
 
-for chunk in pd.read_csv('FC_Score_variable_Sep20.csv', chunksize = chunk_size):
-    chunk.to_csv('FC_Score_variable_Sep20_' + str(batch_no) + '.csv', index = False)
+for chunk in pd.read_csv('File_name.csv', chunksize = chunk_size):
+    chunk.to_csv('Filename' + str(batch_no) + '.csv', index = False)
     batch_no += 1
